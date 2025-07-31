dce5c95a646930fc238b67d098e580c1cf224b2a
unknown <malikraziii58@gmail.com>
2025-07-31 01:10:07 -0700

    product screen implemented

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: AppColors.primary),
       ),
-      home: SplashScreen(),
+      home: DummyTestScreen(),
       initialRoute: '/',
     );
   }
